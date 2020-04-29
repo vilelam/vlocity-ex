7760a0ccd27a2ecd9b2698d6f5139bc35ac3ec26
justin1108 <justinyue@live.com>
2020-04-28 21:53:58 -0700

    Basic UI framework is done for JSON Viewer

--- force-app/main/utilities/lwc/jsonAttributeViewer/jsonAttributeViewer.js
+++ force-app/main/utilities/lwc/jsonAttributeViewer/jsonAttributeViewer.js
@@ -1,24 +1,38 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
+const actions = [
+    { label: 'Details', name: 'show_details' }
+];
 const columns = [
-    { label: 'Code', fieldName: 'code', sortable: true },
+    // { label: 'Code', fieldName: 'code', sortable: true },
     { label: 'Name', fieldName: 'name', sortable: true },
     { label: 'Value', fieldName: 'value', sortable: true },
-    { label: 'Category', fieldName: 'category_name', sortable: true }
-    // { label: 'Website', fieldName: 'website', type: 'url' },
-    // { label: 'Phone', fieldName: 'phone', type: 'phone' },
-    // { label: 'Balance', fieldName: 'amount', type: 'currency' },
-    // { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
+    { label: 'Code', fieldName: 'code', sortable: true },
+    { label: 'Category', fieldName: 'category_name', sortable: true },
+    {
+        type: 'button-icon',
+        typeAttributes: { iconName: 'utility:edit', iconClass: "slds-button__icon", name: 'edit', variant: 'bare' },
+        fixedWidth:30,
+    }
+
 ];
 
 export default class JsonAttributeViewer extends LightningElement {
     @api recordId;
     @api objectApiName;
 
-    @track jsonAttributeField;
-    @track attributes;
-    @track columns = columns;
     @track record;
+    @track jsonAttributeField;
+    // Attribute List
+    @track attributes;
+    
+    // Attribute Info
+    @track attribute;
+
+    @track columns = columns;
+ 
+    tablePanelSize = 6;
+    infoPanelSize = 6;
 
     // jsonAttribute;
 
@@ -54,13 +68,39 @@ export default class JsonAttributeViewer extends LightningElement {
                         active: myAttribute.isactive__c,
                         readonly: myAttribute.isreadonly__c,
                         is_picklist: false,
-                        is_text: false,
-                        is_checkbox: false
+                        is_input: false,
+                        is_number: false,
+                        is_checkbox: false,
+                        is_combobox: false,
                     };
 
                     var myRuntimeInfo = myAttribute.attributeRunTimeInfo;
                     attribute.data_type = myRuntimeInfo.dataType;
-                    if (myRuntimeInfo.dataType === "Picklist") {
+                    if (myRuntimeInfo.dataType === "Currency" ||
+                        myRuntimeInfo.dataType === "Percent" ||
+                        myRuntimeInfo.dataType === "Number") {
+                        attribute.input_type = 'number';
+                        attribute.is_number = true;
+                        attribute.value = myRuntimeInfo.value + "";
+                    } else if (myRuntimeInfo.dataType === "Text" || 
+                        myRuntimeInfo.dataType === "Lookup") {
+                        attribute.input_type = 'text';
+                        attribute.is_input = true;
+                        attribute.value = myRuntimeInfo.value;
+                    } else if (myRuntimeInfo.dataType === "Checkbox") {
+                        attribute.input_type = 'checkbox';                        
+                        attribute.is_checkbox = true;
+                        attribute.value = myRuntimeInfo.value + "";
+                    } else if (myRuntimeInfo.dataType === "Date") {
+                        attribute.input_type = 'date';
+                        attribute.is_input = true;
+                        attribute.value = myRuntimeInfo.value + "";
+                    } else if (myRuntimeInfo.dataType === "Datetime") {
+                        attribute.input_type = 'datetime';
+                        attribute.is_input = true;
+                        attribute.value = myRuntimeInfo.value + "";
+                    } else if (myRuntimeInfo.dataType === "Picklist") {
+                        attribute.is_combobox = true;
                         attribute.value = Object.keys(myRuntimeInfo.selectedItem).length === 0 ? null : myRuntimeInfo.selectedItem.displayText;
                         attribute.options = [];
                         for (var j = 0; j < myRuntimeInfo.values.length; j++) {
@@ -69,29 +109,24 @@ export default class JsonAttributeViewer extends LightningElement {
                                 value: myRuntimeInfo.values[j].value
                             });
                         }
-                        attribute.is_picklist = true;
-                    } else {
-                        if (myRuntimeInfo.dataType === "Checkbox") {
-                            attribute.is_checkbox = true;
-                            attribute.options = [{
-                                label: "true",
-                                value: "true"
-                                },{
-                                    label: "false",
-                                    value: "false"
-                                }
-                            ];
-                            attribute.value = myRuntimeInfo.value + "";
-                        } else {
-                            attribute.is_text = true;
-                            attribute.value = myRuntimeInfo.value;
-                        }
                         
+                    } else if (myRuntimeInfo.dataType === "Multi Picklist") {
+                        attribute.input_type = 'text';
+                        attribute.is_input = true;
+                        attribute.value = myRuntimeInfo.value ;//+ "";
+                    } else {
+                        attribute.input_type = 'text';
+                        attribute.is_input = true;
+                        attribute.value = myRuntimeInfo.value ;//+ "";
                     }
+                    // attribute.value = myRuntimeInfo.value ;//+ "";
+
                     this.attributes.push(attribute);
                 }
 
             }
+
+            console.log(this.attributes);
         }
     }
 
@@ -100,5 +135,21 @@ export default class JsonAttributeViewer extends LightningElement {
             return this.record.fields.vlocity_cmt__JSONAttribute__c.value;
         else
             return 'Record not found';
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        this.attribute = event.detail.row;
+        console.log(row);
+        switch (actionName) {
+            case 'delete':
+                // alert('delete');
+                break;
+            case 'show_details':
+                // alert('details');
+                break;
+            default:
+        }
     }
 }
